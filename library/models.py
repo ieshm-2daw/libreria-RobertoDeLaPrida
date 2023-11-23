9ed6d9d8ae7693bfd5a21662d123b3c87bf0130a
RobertoDeLaPrida <rdelpul993@gmail.com>
2023-11-23 12:56:19 +0100

    Error en makemigrations

--- library/models.py
+++ library/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
 # Create your models here.
 
-class User(AbstractUser):
 
+
+class CustomUser(AbstractUser):
     dni = models.CharField(max_length=10)
     direcction = models.CharField(max_length=100)
-    telf = models.PositiveIntegerField(max_length=9)
+    telf = models.PositiveIntegerField()
+
     
+
     def __str__(self):
         return self.username
